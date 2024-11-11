f2d9d284cf2e3c8c910ee541a13e37b9f7f34458
chicken-04 <henryctran2007@gmail.com>
2024-11-11 15:31:30 -0600

    saving current code

--- main.ino
+++ main.ino
@@ -1,19 +1,27 @@
 #include <Servo.h>
 #include "Gizmo.h"
 
-Servo DriveL, DriveR, Arm;
+Servo DriveL, DriveR, Arm, ClawL, ClawR, Dispenser;
 Gizmo gizmo;
 
 void setup() {
   gizmo.begin();
+  Serial.begin(9600);
 
   pinMode(GIZMO_MOTOR_1, OUTPUT);
   pinMode(GIZMO_MOTOR_2, OUTPUT);
   pinMode(GIZMO_MOTOR_3, OUTPUT);
 
+  pinMode(GIZMO_SERVO_1, OUTPUT);
+  pinMode(GIZMO_SERVO_2, OUTPUT);
+  pinMode(GIZMO_SERVO_3, OUTPUT);
+
   DriveL.attach(GIZMO_MOTOR_1);
   Arm.attach(GIZMO_MOTOR_2);
   DriveR.attach(GIZMO_MOTOR_3);
+  ClawL.attach(GIZMO_SERVO_1);
+  ClawR.attach(GIZMO_SERVO_2);
+  Dispenser.attach(GIZMO_SERVO_3);
 
   pinMode(LED_BUILTIN, OUTPUT);
 }
@@ -30,11 +38,21 @@ void loop() {
   bool targetArmUp, targetArmDown;
   targetArmDown = gizmo.getButton(GIZMO_BUTTON_LT);
   targetArmUp = gizmo.getButton(GIZMO_BUTTON_RT);
-  int targetArm = targetArmDown ? 0 : targetArmUp ? 180 : 90;
+  int targetArm = targetArmDown ? 180 : targetArmUp ? 0 : 90;
+
+  // bool targetClawL, targetClawR;
+  // targetClawL = gizmo.getButton(GIZMO_BUTTON_LSHOULDER);
+  // targetClawR = gizmo.getButton(GIZMO_BUTTON_RSHOULDER);
+  // int targetClaw = targetArmDown ? 0 : targetArmUp ? 90 : 180;
+
+  // bool targetDispenser;
+  // targetDispenser = gizmo.getButton(GIZMO_BUTTON_B);
+  // int targetDispense = targetDispenser ? 180 : 90;
 
   DriveL.write(targetL);
   DriveR.write(targetR);
   Arm.write(targetArm);
+  // Dispenser.write(targetDispense);
 
   delay(20);
 }
