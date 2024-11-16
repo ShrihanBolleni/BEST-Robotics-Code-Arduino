8ea61788ef5e70c39693330b76bf6bbf60c525ec
chicken-04 <henryctran2007@gmail.com>
2024-11-16 09:24:08 -0600

    gameday v1 code

--- main.ino
+++ main.ino
@@ -21,18 +21,21 @@ void setup() {
   Arm.attach(GIZMO_MOTOR_2);
   DriveR.attach(GIZMO_MOTOR_3);
 
-  Dispenser.attach(21);
+  Dispenser.attach(21, 1000, 2100);
   ClawL.attach(GIZMO_SERVO_1);
   ClawR.attach(GIZMO_SERVO_2);
-  Manny.attach(GIZMO_SEROVO_3);
+  Manny.attach(GIZMO_SERVO_3, 700, 2300);
 
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 int targetClawL = 90;
 int targetClawR = 90;          // Servo position (0 or 90 degrees)
-int targetDispenser = 10;
+int targetDispenser = 0;
 int targetManny = 0;
+// Variables to store previous button states
+bool lastDispensingState = false;
+bool lastSeatBeltState = false;
 
 void loop() {
   digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
@@ -58,28 +61,41 @@ void loop() {
   } else if(rightBump){
     targetClawL = 0;
     targetClawR = 180;
-    Serial.println("test");
     delay(20);
   }
 
+  // Check if dispensing button has been pressed (on rising edge)
   bool dispensing = gizmo.getButton(GIZMO_BUTTON_B);
-  if(dispensing){
-    Dispenser.write(110);
-    delay(20);
+  if (dispensing && !lastDispensingState) {
+    targetDispenser = targetDispenser == 0 ? 180 : 0;  // Toggle dispenser
+    delay(50);
+  }
+  lastDispensingState = dispensing;  // Update last state
+
+  if(targetDispenser == 180){
+    Dispenser.writeMicroseconds(2100);
+  } else {
+    Dispenser.writeMicroseconds(1000);
   }
 
+  // Check if seatBelt button has been pressed (on rising edge)
   bool seatBelt = gizmo.getButton(GIZMO_BUTTON_X);
-  if(seatBelt){
-    targetManny = 180;
-    delay(20);
+  if (seatBelt && !lastSeatBeltState) {
+    targetManny = targetManny == 0 ? 180 : 0;  // Toggle Manny
+    delay(50);
+  }
+  lastSeatBeltState = seatBelt;  // Update last state
+
+  if(targetManny == 180){
+    Manny.writeMicroseconds(2300);
+  } else {
+    Manny.writeMicroseconds(1500);
   }
 
-  DriveL.write(targetL);
-  DriveR.write(targetR);
+  DriveR.write(targetL);
+  DriveL.write(targetR);
   Arm.write(targetArm);
   ClawL.write(targetClawL);
   ClawR.write(targetClawR);
-  Manny.write(targetManny);
-
   delay(20);
 }
